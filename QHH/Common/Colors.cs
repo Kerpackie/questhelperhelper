acb6c159158637b3392087fca5989a2d7e54bde0
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-09-15 15:08:34 +0100

    Update Colors.cs
    Update values to various custom colours.
    
    Add 25% Light and Dark versions of Quest and Diary Colours.

--- QHH/Common/Colors.cs
+++ QHH/Common/Colors.cs
@@ -1,30 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Discord;
-
-namespace QHH.Common
+﻿namespace QHH.Common
 {
+    using Discord;
+
+    /// <summary>
+    /// Colors used by QHH.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "Fields are used throughout application.")]
     public static class Colors
     {
+        /// <summary>
+        /// The color of Discord.
+        /// </summary>
         public static Color Discord = new Color(114, 137, 218);
 
+        /// <summary>
+        /// The color used to indicate an informative state.
+        /// </summary>
         public static Color Information = new Color(26, 155, 226);
 
+        /// <summary>
+        /// The color used to indicate a success state.
+        /// </summary>
         public static Color Success = new Color(95, 218, 153);
 
+        /// <summary>
+        /// The color used to indicate an error state.
+        /// </summary>
         public static Color Error = new Color(236, 56, 69);
 
+        /// <summary>
+        /// The color used to indicate a warning state.
+        /// </summary>
         public static Color Warning = new Color(254, 184, 6);
 
+        /// <summary>
+        /// The color used to indicate an active state.
+        /// </summary>
         public static Color Active = new Color(254, 200, 16);
 
-        //TODO: UPDATE COLOURS FOR QUEST AND DIARY.
+        /// <summary>
+        /// The primary color in the Quest Icon.
+        /// </summary>
+        public static Color Quest = new Color(34, 44, 75);
 
-        public static Color Quest = new Color(0, 0, 0);
+        /// <summary>
+        /// The primary color in the Quest Icon 25% Lighter.
+        /// </summary>
+        public static Color Quest25Light = new Color(54, 61, 82);
 
-        public static Color Diary = new Color(0, 0, 0);
+        /// <summary>
+        /// The primary color in the Quest Icon 25% Darker.
+        /// </summary>
+        public static Color Quest25Dark = new Color(62, 87, 160);
+
+        /// <summary>
+        /// The primary color in the Diary Icon.
+        /// </summary>
+        public static Color Diary = new Color(44, 128, 62);
+
+        /// <summary>
+        /// The primary color in the Diary Icon 25% Lighter.
+        /// </summary>
+        public static Color Diary25Light = new Color(55, 160, 78);
+
+        /// <summary>
+        /// The primary color in the Diary Icon 25% Darker.
+        /// </summary>
+        public static Color Diary25Dark = new Color(35, 102, 50);
     }
 }
