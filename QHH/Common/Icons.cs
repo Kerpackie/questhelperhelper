c2edd9e0fb625a251200a531c1c45f9c6fd844f5
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-09-15 13:42:28 +0100

    Update Icons.cs
    Add URLs for icons and thumbnails.

--- QHH/Common/Icons.cs
+++ QHH/Common/Icons.cs
@@ -1,27 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QHH.Common
+﻿namespace QHH.Common
 {
+    /// <summary>
+    /// Icons and thumbnails used by QuestHelperHelper
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "Fields are used throughout application.")]
     public static class Icons
     {
-        public static string Success = "";
+        /// <summary>
+        /// Icon used to indicate a success state.
+        /// </summary>
+        public static string Success = "https://i.imgur.com/It9BNU8.png";
 
-        public static string Error = "";
+        /// <summary>
+        /// Icon used to indicate an error state.
+        /// </summary>
+        public static string Error = "https://i.imgur.com/mNCmgf3.png";
 
-        public static string Warning = "";
+        /// <summary>
+        /// Icon used to indicate a Warning state.
+        /// </summary>
+        public static string Warning = "https://i.imgur.com/X6M1yE7.png";
 
-        public static string Restricted = "";
+        /// <summary>
+        /// Icon used to indicate a restricted state.
+        /// </summary>
+        public static string Restricted = "https://i.imgur.com/mTUfdsc.png";
 
-        public static string Wait = "";
+        /// <summary>
+        /// Icon used to indicate a loading state.
+        /// </summary>
+        public static string Wait = "https://i.imgur.com/XWK6FIH.png";
 
-        public static string Information = "";
+        /// <summary>
+        /// Icon used to indicate an information state.
+        /// </summary>
+        public static string Information = "https://i.imgur.com/gLR4k7d.png";
 
-        public static string Diary = "";
+        /// <summary>
+        /// Thumbail used for Diaries.
+        /// </summary>
+        public static string Diary = "https://cdn.discordapp.com/attachments/854379120624271380/881270915894247444/Achievement_Diaries.png";
 
-        public static string Quest = "";
+        /// <summary>
+        /// Thumbnail used for Quests..
+        /// </summary>
+        public static string Quest = "https://cdn.discordapp.com/attachments/854379120624271380/887373772900958288/802308810420387861.png";
     }
 }
