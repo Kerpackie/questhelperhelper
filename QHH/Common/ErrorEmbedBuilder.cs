b174e633bbe041414de0e0b11f5151888f743caa
Kerpackie <81828282+Kerpackie@users.noreply.github.com>
2021-09-15 16:04:08 +0100

    Create ErrorEmbedBuilder.cs
    Temporary implementation during refactoring.

---
+++ QHH/Common/ErrorEmbedBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace QHH.Common
+{
+    using Discord;
+
+    /// <summary>
+    /// An error embed builder, will move over whatever uses this to QHHEmbedBuilder eventually.
+    /// </summary>
+    internal class ErrorEmbedBuilder : EmbedBuilder
+    {
+        public ErrorEmbedBuilder()
+        {
+            this.WithTitle("ERROR!");
+            this.WithColor(Colors.Error);
+        }
+    }
+}
